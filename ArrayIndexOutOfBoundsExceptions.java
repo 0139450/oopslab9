424303d43b72d58b50ec493c12eca7c63e2a53eb
0139450 <159297417+0139450@users.noreply.github.com>
2024-04-18 08:14:20 +0530

    Create ArrayIndexOutOfBoundsExceptions.java

---
+++ ArrayIndexOutOfBoundsExceptions.java
@@ -0,0 +1,16 @@
+class  ArrayIndexOutOfBoundsExceptions{
+    
+    public static void main(String args[])
+    {
+        try
+        {
+            int a[]=new int[5];
+            a[6]=9;
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("array index is out of bound");
+        }
+        
+    }
+}
